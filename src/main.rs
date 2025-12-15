95acc7ceb99fcbf25d691887964a3e808009f101
pool2win <78231905+pool2win@users.noreply.github.com>
2025-12-15 15:18:40 +0100

    Specify rustfmt edition 2024

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use app::App;
 use crossterm::{
     event::{self, Event, KeyCode, KeyEventKind},
     execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
-use ratatui::{backend::CrosstermBackend, Terminal};
+use ratatui::{Terminal, backend::CrosstermBackend};
 use std::io;
 
 fn main() -> Result<()> {
